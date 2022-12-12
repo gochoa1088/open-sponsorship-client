d71a01a2f0028c5ed72980f4327dc1d51029a049
gochoa1088 <gochoa1088@gmail.com>
2022-12-12 01:20:07 -0800

    image updates

--- src/components/AthleteInfo.js
+++ src/components/AthleteInfo.js
@@ -3,42 +3,45 @@ import React from "react";
 const AthleteInfo = ({ info }) => {
   return (
     <div className="container my-5 bg-light d-flex flex-column align-items-center">
-      <p>
-        <span className="fw-bold">Name: </span>
-        {info.name}
-      </p>
-      <p>
-        <span className="fw-bold">Date of Birth: </span>
-        {info.dob}
-      </p>
-      <p>
-        <span className="fw-bold">Location: </span>
-        {info.location}
-      </p>
-      <p>
-        <span className="fw-bold">Sports: </span>
-        {info.sports.join(", ")}
-      </p>
-      <p>
-        <span className="fw-bold">Location: </span>
-        {info.location}
-      </p>
-      <p>
-        <span className="fw-bold">Team: </span>
-        {info.team}
-      </p>
-      <p>
-        <span className="fw-bold">About: </span>
-        {info.about}
-      </p>
-      <p>
-        <span className="fw-bold">Interests: </span>
-        {info.interests}
-      </p>
-      <p>
-        <span className="fw-bold">Profile Image: </span>
-        {info.profileImage}
-      </p>
+      <img
+        src={info.profileImage}
+        style={{ height: "180px" }}
+        className="img-thumbnail img-responsive align-self-center my-4"
+      />
+      <div className="d-flex flex-column align-items-center">
+        <p>
+          <span className="fw-bold">Name: </span>
+          {info.name}
+        </p>
+        <p>
+          <span className="fw-bold">Date of Birth: </span>
+          {info.dob}
+        </p>
+        <p>
+          <span className="fw-bold">Location: </span>
+          {info.location}
+        </p>
+        <p>
+          <span className="fw-bold">Sports: </span>
+          {info.sports.join(", ")}
+        </p>
+        <p>
+          <span className="fw-bold">Location: </span>
+          {info.location}
+        </p>
+        <p>
+          <span className="fw-bold">Team: </span>
+          {info.team}
+        </p>
+        <p>
+          <span className="fw-bold">About: </span>
+          {info.about}
+        </p>
+        <p>
+          <span className="fw-bold">Interests: </span>
+          {info.interests}
+        </p>
+      </div>
     </div>
   );
 };
